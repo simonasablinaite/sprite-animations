51cb99e42482d07911a86edc6dce9bb22067fc41
Simona <simonasablinaite@gmail.com>
2024-10-30 16:08:12 +0200

    A data structure representing locations on a sprite sheet

--- script.js
+++ script.js
@@ -12,21 +12,46 @@ let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 
+const spriteAnimations = [];
+const animationStates = [
+   {
+      name: 'idle',
+      frames: 7,
+   },
+   {
+      name: 'jump',
+      frames: 7,
+   }
+];
+
+animationStates.forEach((state, index) => {
+   let frames = {
+      location: [],
+   }
+
+   for (let j = 0; j < state.frames; s++) {
+      let positionX = j * spriteWidth;
+      let positionY = index * spriteHeight; frames.location.push({ x: positionX, y: positionY });
+   }
+   spriteAnimations[state.name] = frames;
+});
+
 // Cia bus programuojamas animacijos ciklas:
 function animate() {
    // Nereikalingu spalvu pasalinimas is canva tarp kiekvieno animacijos kadro
    context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+   // Nustatomas kadras Y i 0:
+   let position = Math.floor(gameFrame / staggerFrames) % 6;
+   frameX = spriteWidth * position;
+
 
    // PRADEDAME PIESTI:
-   // context.fillRect(50, 50, 100, 100);
-
    // Piesimo metodas priima 3, 5 arba 9 parametrus, priklausomai kiek norima valdyti vaizda:
-   // context.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-   context.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-   if (gameFrame % staggerFrames === 0) {
-      if (frameX < 6) frameX++;
-      else frameX = 0;
-   }
+   context.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+   // Metodai, leidziantys pereiti tarp skirtingu animacijos busenu:
+
+
 
 
    gameFrame++;
